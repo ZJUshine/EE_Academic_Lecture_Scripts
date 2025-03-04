2cba476f51eaba50a516e76ac1a7ca045e256f12
zjushine-mac-mini <3190103641@zju.edu.cn>
2025-03-04 20:00:57 +0800

    适应新版本

--- main.py
+++ main.py
@@ -17,19 +17,24 @@ driver.get(link1)
 
 # 选择浙大通行证登录
 driver.find_element(By.XPATH, "/html/body/div/div/form/div[2]").click()
+# 点击登陆
+denglu = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div/div/div[2]/form/button")))
+denglu.click()
 # 输入账号密码
 driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div/div[2]/div/div[1]/form/div[1]/div/input[1]").send_keys(id_number)
 driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div/div[2]/div/div[1]/form/div[2]/div/input").send_keys(password)
 # 登录
 driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div/div[2]/div/div[1]/form/div[4]/div[1]").click()
-# 进入讲座列表
-element_huodongliebiao = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div[1]/div[1]/div/ul/div[1]/li/ul/a[3]")))
-element_huodongliebiao.click()
-# 按时间先后顺序排序
-element_paixu = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div[2]/section/div/div[3]/div[2]/table/thead/tr/th[4]/div/span/i[2]")))
-element_paixu.click()
+# 点击学术活动
+xueshuhuodong = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div[2]/aside/div/div[1]/ul/li[2]/div/span/div")))
+xueshuhuodong.click()
+
+# 点击活动列表
+huodongliebiao = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div[2]/aside/div/div[1]/ul/li[2]/ul/li[1]/span/a")))
+huodongliebiao.click()
+
 # 获取最新的讲座时间
-element_huodongshijain = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[2]/section/div/div[3]/div[3]/table/tbody/tr[1]/td[4]")))
+element_huodongshijain = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div[2]/div[2]/main/div/div[2]/div/div/div/div/div/div[2]/div/div/div/div/div/table/tbody/tr[1]/td[4]")))
 time_start = element_huodongshijain.text
 time_start = time_start + ":00"
 print("报名时间：",time_start)
@@ -43,11 +48,16 @@ while True:
 
 
 try:
-    element_baoming = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[2]/section/div/div[3]/div[3]/table/tbody/tr[1]/td[8]/div/button")))
+    element_baoming = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/main/div/div[2]/div/div/div/div/div/div[2]/div/div/div/div/div/table/tbody/tr[1]/td[8]/div/button/span")))
     element_baoming.click()
-    element_sure = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div[3]/button[2]")))
+    
+    # 等待并点击确定按钮
+    element_sure = WebDriverWait(driver,10).until(
+        EC.element_to_be_clickable(
+            (By.XPATH, "//div[@role='dialog']//button[contains(., '确定')]")
+        )
+    )
     element_sure.click()
     print("成功...")
-except:
-    print("失败...")
-    pass
+except Exception as e:
+    print("失败:", str(e))
